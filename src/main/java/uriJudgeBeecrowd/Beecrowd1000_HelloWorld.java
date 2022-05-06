faade2254669162fd51a9d8f5b79252a9fae4494
André Basseto <andre.basseto@hotmail.com>
2022-05-06 00:38:01 -0300

    exercicio 1000

---
+++ src/main/java/uriJudgeBeecrowd/Beecrowd1000_HelloWorld.java
@@ -0,0 +1,14 @@
+package uriJudgeBeecrowd;
+
+/**
+ *
+ * @author andrebasseto
+ *
+ */
+public class Beecrowd1000_HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
