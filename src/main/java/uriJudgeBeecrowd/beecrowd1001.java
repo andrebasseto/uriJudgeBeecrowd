7172cfaea5d2c426e3474cc6826a5d8ce16bd4b6
André Basseto <andre.basseto@hotmail.com>
2022-05-06 01:09:44 -0300

    exercicio 1001

---
+++ src/main/java/uriJudgeBeecrowd/beecrowd1001.java
@@ -0,0 +1,22 @@
+package uriJudgeBeecrowd;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author andrebasseto
+ */
+public class beecrowd1001 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int a, b, x;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        x = a + b;
+        System.out.println("X = " + x);
+        
+        
+    }
+    
+}
